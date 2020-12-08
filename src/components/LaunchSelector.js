7d205ea5f8827f99a1871fe3d9033e574808b25d
Your Name <mmuzasantuna@gmail.com>
2020-12-08 15:06:27 +0000

    props for delectedLaunchId passed to launchSelector

--- src/components/LaunchSelector.js
+++ src/components/LaunchSelector.js
@@ -1,8 +1,16 @@
-const LaunchSelector = () => {
+const LaunchSelector = ( {increment, decrement}) => {
 
     return (
-        <p>Launch selector here</p>
+        <>
+          <button onClick = {decrement}> Previous Launch</button>
+          <button onClick = {increment}>Next Launch</button>
+        </>
     )
 }
 
 export default LaunchSelector;
+
+// Add a method decrementSelectedLaunch to decrease selectedLaunchId by 1
+// Pass the functions in props to LaunchSelector
+// Add onClick listeners to the buttons which will call the functions passed down
+// There have only been 90 SpaceX launches to date. Add guards to the functions to ensure the user can’t ask for a launch that doesn’t exist.
--- src/containers/LaunchContainer.js
+++ src/containers/LaunchContainer.js
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
 import LaunchDetails from "../components/LaunchDetails";
 import LaunchSelector from "../components/LaunchSelector";
 
 const LaunchContainer = () => {
 
+    const [launch, setLaunch] = useState({});
+    const [selectedLaunchId, setSelectedLaunchId] = useState(1);
+
+
+    const getLaunch = () => {
+        console.log("getting launch info");
+        fetch(`https://api.spacexdata.com/v3/launches/${selectedLaunchId}`)
+            .then(res => res.json())
+            .then(data => setLaunch(data))
+    }
+
+    useEffect(() => {
+        getLaunch()
+    }, []);
+
+    const incrementSelectedLaunch = () => {
+        if( selectedLaunchId < 90) {
+            setSelectedLaunchId(selectedLaunchId + 1)
+        }
+    }
+
+    const decreaseSelectedLaunchId = () => {
+        if( selectedLaunchId > 1) {
+            setSelectedLaunchId(selectedLaunchId - 1)
+        }
+    }
+
     return (
         <>
         <h1>SpaceX Launch Details</h1>
-        <LaunchSelector/>
-        <LaunchDetails />
+        <LaunchSelector increment = {incrementSelectedLaunch} decrement ={decreaseSelectedLaunchId}/>
+        <LaunchDetails  />
         </>
     )
 }
